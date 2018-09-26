4594f73fa82c0981671ee7185ef9e05f29aee356
salmask <40783416+salmask@users.noreply.github.com>
2018-09-26 11:10:28 +0530

    Add files via upload

--- Receive.java
+++ Receive.java
@@ -1,37 +1,38 @@
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
-import java.util.concurrent.TimeoutException;
 
-import com.rabbitmq.client.AMQP;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Consumer;
-import com.rabbitmq.client.DefaultConsumer;
-import com.rabbitmq.client.Envelope;
 public class Receive {
-	  private final static String QUEUE_NAME = "kello";
 
-	  public static void main(String[] argv)
-	      throws java.io.IOException,
-	             java.lang.InterruptedException, TimeoutException {
+    private static final String EXCHANGE_NAME = "direct_logs";
 
-	    ConnectionFactory factory = new ConnectionFactory();
-	    factory.setHost("localhost");
-	    Connection connection = factory.newConnection();
-	    Channel channel = connection.createChannel();
+    public static void main(String[] argv) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        Connection connection = factory.newConnection();
+        Channel channel = connection.createChannel();
 
-	    channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-	    System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
-	    Consumer consumer = new DefaultConsumer(channel) {
-	    	  @Override
-	    	  public void handleDelivery(String consumerTag, Envelope envelope,
-	    	                             AMQP.BasicProperties properties, byte[] body)
-	    	      throws IOException {
-	    	    String message = new String(body, "UTF-8");
-	    	    System.out.println(" [x] Received '" + message + "'");
-	    	  }
-	    	};
-	    	channel.basicConsume(QUEUE_NAME, true, consumer);
-	    }
-	}
+        channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+        String queueName = channel.queueDeclare().getQueue();
+
+        if (argv.length < 1){
+            System.err.println("Usage: ReceiveLogsDirect [info] [warning] [error]");
+            System.exit(1);
+        }
+
+        for(String severity : argv){
+            channel.queueBind(queueName, EXCHANGE_NAME, severity);
+        }
+        System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        Consumer consumer = new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope,
+                                       AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + envelope.getRoutingKey() + "':'" + message + "'");
+            }
+        };
+        channel.basicConsume(queueName, true, consumer);
+    }
+}
